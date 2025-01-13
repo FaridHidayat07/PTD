8fbe11bd0b6fb8018a4d8724d21bb03e8aeb8415
FaridHidayat07 <muhammad_farid_hidayat@teknokrat.ac.id>
2025-01-13 15:41:48 +0700

    muhammadfaridhidayat : membuat halaman tabel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/footer/page";
 import Gallery from "@/components/gallery/page";
 import Header from "@/components/header/page";
 import Navbar from "@/components/Navbar/page";
-// import CurrencyTable from "@/components/tabel/page";
+import CurrencyTable from "@/components/tabel/page";
 import Image from "next/image";
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
       <Navbar/>
       <Header/>
       <About/>
-      {/* <CurrencyTable/> */}
+      <CurrencyTable/>
       <Gallery/>
       <Footer/>
     </>
