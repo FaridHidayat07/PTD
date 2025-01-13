a77ff7a85f0dbc2fff44172e9d824de423b8b617
FaridHidayat07 <muhammad_farid_hidayat@teknokrat.ac.id>
2025-01-13 15:36:30 +0700

    muhammadfaridhidayat : membuat halaman about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import About from "@/components/About/page";
 // import Footer from "@/components/footer/page";
 // import Gallery from "@/components/gallery/page";
-// import Header from "@/components/header/page";
+import Header from "@/components/header/page";
 import Navbar from "@/components/Navbar/page";
 // import CurrencyTable from "@/components/tabel/page";
 import Image from "next/image";
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <Navbar/>
-      {/* <Header/> */}
+      <Header/>
       <About/>
       {/* <CurrencyTable/>
       <Gallery/>
